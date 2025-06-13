25c2a8caac2a3ddcf8dd254508927cb4c8e7667e
이은정 <ej2grateful@gmail.com>
2025-06-13 10:31:52 +0900

    first

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+class Person {
+    public String name;
+}
+public class Main {
+    public static void main(String[] args) {
+
+        Person person = new Person();
+        person.name = "홍길동";
+
+        System.out.println(person.name);
+
+    }
+}
